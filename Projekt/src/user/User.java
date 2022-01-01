584f65a8bb5a438812a62644df05d01e2838a5c8
lobna99 <lobna.bou23@gmail.com>
2022-01-01 18:19:16 +0100

    user package

--- Projekt/src/user/User.java
+++ Projekt/src/user/User.java
@@ -1,11 +1,21 @@
+package user;
+
+import lombok.Getter;
+
 public class User {
+    @Getter
     private String Username;
+    @Getter
     private String Password;
+    @Getter
+    private int ELO;
+    @Getter
     private int Coins;
 
     public User(String Username,String Password){
         this.Username=Username;
         this.Password=Password;
+        this.ELO=100;
         this.Coins=20;
     }
     public void purchasePack(){
