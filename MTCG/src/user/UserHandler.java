569a00da9a13a121a98347ef3d3a68414ca24d35
lobna99 <lobna.bou23@gmail.com>
2022-01-08 00:55:11 +0100

    Update user

--- MTCG/src/user/UserHandler.java
+++ MTCG/src/user/UserHandler.java
@@ -7,6 +7,7 @@ import server.Responsebuilder;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserHandler {
@@ -41,5 +42,58 @@ public class UserHandler {
         statement.setInt(4, newUser.getCoins());
         statement.execute();
         statement.close();
+    }
+    public void getUserData(String user){
+            PreparedStatement statement = null;
+            try {
+                statement = Connection.getConnection().prepareStatement("""
+                             SELECT *
+                             from users
+                             WHERE username=?
+                         """);
+                statement.setString(1, user);
+                ResultSet rs=statement.executeQuery();
+                while(rs.next()){
+                    String username=rs.getString("Username");
+                    String name=rs.getString("name");
+                    String bio=rs.getString("bio");
+                    String image=rs.getString("image");
+                    int elo=rs.getInt("elo");
+                    int coins=rs.getInt("coins");
+                    String response="{\"Username\":\""+username+"\",\"Name\":\""+name+"\",\"ELO\":\""+elo+"\",\"Coins\":\""+coins+"\",\"Bio\":\""+bio+"\",\"Image\":\""+image+"\"}";
+                    try {
+                        respond.writeHttpResponse(HttpStatus.OK,response);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+    public void updateUser(String user,JsonNode node){
+        PreparedStatement statement = null;
+        try {
+            statement = Connection.getConnection().prepareStatement("""
+                                UPDATE users
+                                 SET bio=?,name=?,image=?
+                                 WHERE username=?;
+                             """);
+            statement.setString(1,node.get("Bio").getValueAsText());
+            statement.setString(2,node.get("Name").getValueAsText());
+            statement.setString(3,node.get("Image").getValueAsText());
+            statement.setString(4,user);
+            statement.execute();
+            statement.close();
+            respond.writeHttpResponse(HttpStatus.OK,"user updated");
+        } catch (SQLException | IOException e) {
+            try {
+                respond.writeHttpResponse(HttpStatus.BAD_REQUEST,"ERR");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
     }
 }
