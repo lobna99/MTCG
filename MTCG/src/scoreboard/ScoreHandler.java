d2d84fba0bdec0f643d79234fc0df463bfee7995
lobna99 <lobna.bou23@gmail.com>
2022-01-08 13:48:30 +0100

    scoreboard works

--- MTCG/src/scoreboard/ScoreHandler.java
+++ MTCG/src/scoreboard/ScoreHandler.java
@@ -1,5 +1,71 @@
 package scoreboard;
 
+import db.DBconnection;
+import server.HttpStatus;
+import server.Responsebuilder;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ScoreHandler {
 
+    private Responsebuilder respond = Responsebuilder.getInstance();
+    private DBconnection Connection;
+
+    public ScoreHandler() {
+        Connection = DBconnection.getInstance();
+    }
+
+
+    public void getScore(String user) {
+        PreparedStatement statement = null;
+        try {
+            statement = Connection.getConnection().prepareStatement("""
+                        SELECT elo
+                        from users
+                        WHERE username=?
+                    """);
+            statement.setString(1, user);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                int elo = rs.getInt("elo");
+                String response = "{\"ELO\":\"" + elo + "\"}";
+                try {
+                    respond.writeHttpResponse(HttpStatus.OK, response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showScoreboard(){
+        PreparedStatement statement = null;
+        try {
+            statement = Connection.getConnection().prepareStatement("""
+                        SELECT elo,username
+                        from users
+                        ORDER BY elo
+                        DESC ;
+                    """);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                int elo = rs.getInt("elo");
+                String username = rs.getString("username");
+                String response = "{\"Username\":\""+username+"\",\"ELO\":\"" + elo + "\"}";
+                try {
+                    respond.writeHttpResponse(HttpStatus.OK, response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
