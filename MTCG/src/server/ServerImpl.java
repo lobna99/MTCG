6bddcbfbc5766271841367b14d24f7e4b3f18612
lobna99 <lobna.bou23@gmail.com>
2022-01-23 15:02:42 +0100

    use Threadpool

--- MTCG/src/server/ServerImpl.java
+++ MTCG/src/server/ServerImpl.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerImpl implements Server {
 
     private ServerSocket _listener;
     private int port;
+    private ExecutorService pool=Executors.newSingleThreadExecutor();
 
     public ServerImpl(int port) {
         this.port = port;
     }
 
     public void start() throws IOException {
-        Runtime.getRuntime().addShutdownHook(new Thread(this::close));
         this.listen();
     }
 
@@ -25,24 +27,14 @@ public class ServerImpl implements Server {
             _listener = new ServerSocket(this.port, 5);
             System.out.println("http-server running at: http://localhost:" + this.port);
             while (true) {
-                Socket s = _listener.accept();
+                Socket s = _listener.accept();;
                 RequestHandlerImpl requestHandler = new RequestHandlerImpl(s);
-                Thread thread = new Thread(requestHandler);
-                thread.start();
-                clients.add(requestHandler);
+                pool.execute(requestHandler);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void close() {
-        System.out.println("Closing on port " + _listener.getLocalPort() + "...");
-        try {
-            _listener.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
 
